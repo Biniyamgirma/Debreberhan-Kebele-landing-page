708983849774c676ea16aae0235ccd96d417e3ac
Biniyamgirma <biniyam0928@gmail.com>
2026-03-11 13:42:26 +0300

    added google connection

--- backend/routes/test/testRoute.js
+++ backend/routes/test/testRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const route = express.Router();
-// const { connectWithConnector } = require("../../config/config");
+const { connectWithConnector } = require("../../config/config");
+const { data } = require("react-router-dom");
 // const { hashPassword } = require("../../utils/utils");
 route.get("/", async (req, res) => {
   try {
-    // pool = await connectWithConnector();
-    // const [rows] = await pool.query("SELECT NOW()");
-    res.json({ message: "its working fine" });
+    const pool = await connectWithConnector();
+    const [rows] = await pool.query("SELECT NOW()");
+    res.json({ message: "its working fine", data: rows });
   } catch (error) {
     console.error("Error connecting to the database:", error.message);
   }
