4a291a40907b76b4a1c40f82809a13a03a84aac0
Biniyamgirma <biniyam0928@gmail.com>
2026-03-11 14:00:01 +0300

    expermnting to fix the error in google database connection

--- backend/routes/test/testRoute.js
+++ backend/routes/test/testRoute.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const route = express.Router();
 const { connectWithConnector } = require("../../config/config");
-const { data } = require("react-router-dom");
 // const { hashPassword } = require("../../utils/utils");
 route.get("/", async (req, res) => {
   try {
     const pool = await connectWithConnector();
     const [rows] = await pool.query("SELECT NOW()");
-    res.json({ message: "its working fine", data: rows });
+    res.end({ message: "its working fine", data: rows });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error", data: error });
-    console.error("Error connecting to the database:", error.message);
   }
 });
 
