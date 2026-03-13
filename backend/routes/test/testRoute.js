e7d5814dd7b369e26e6fd5afe9e0c0958523b5ed
Biniyamgirma <biniyam0928@gmail.com>
2026-03-13 11:39:17 +0300

    test deployment

--- backend/routes/test/testRoute.js
+++ backend/routes/test/testRoute.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const route = express.Router();
 const sql = require("../../config/db");
-const { hashPassword } = require("../../utils/utils");
 const supabase = require("../../config/supabaseClient");
 route.get("/", async (req, res) => {
   try {
-    const { data, error } = await supabase.from("user").select("*");
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-    res.json(data);
+    // const { data, error } = await supabase.from("user").select("*");
+    // if (error) {
+    //   return res.status(500).json({ error: error.message });
+    // }
+    // res.json(data);
+    res.json({ message: "hello" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error", data: error });
   }
